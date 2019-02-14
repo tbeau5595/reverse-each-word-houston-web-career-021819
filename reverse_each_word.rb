2573c438218716029ba60b1fd2afad79a7fcd66c
Trey <tbeau5595@hotmail.com>
2019-02-14 22:00:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
-  words = sentence.split(" ")
+  words = sentence.split
   words.collect do |word|
     word.reverse!
   end
-  words.join
+  words.join(" ")
 end
   
